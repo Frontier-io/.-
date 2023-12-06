e0768e5642aa59f664b1deb151d22922aa43c8e2
Frontier-io <152659769+Frontier-io@users.noreply.github.com>
2023-12-06 01:49:46 +0100

    Delete login.frontier.com/ftr-sys-theme directory

--- login.frontier.com/ftr-sys-theme/js/jump.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  // Jump to other web sites.
-  $("#jump_selection").change(function() {
-    var newv = $(this).val();
-    if (newv && newv.length > 0) {
-      window.location.href = newv;
-    }
-  });
-});
